8149329c2bdc87ba82456e2c19a92168d8db57a3
ThomasHoek <thomas.hoek@student.hu.nl>
2019-12-17 14:44:11 +0100

    vissen en monte carlo plaatjes

--- modelling/week3/vissen.py
+++ modelling/week3/vissen.py
@@ -5,42 +5,58 @@ maximum_growth_rate = 0.5 # 1 / year
 carrying_capacity = 2e6 # tons
 maximum_harvest_rate = 0.7 * 2.5e5 # tons / year
 ramp_start = 2. # years
-ramp_end = 6. # years
+ramp_end = 6 # years
 
 end_time = 10. # years
 h = 0.1 # years
 num_steps = int(end_time / h)
 times = h * numpy.array(range(num_steps + 1))
 
-def harvest():
+def total_harvest():
     fish = numpy.zeros(num_steps + 1) # tons
     fish[0] = 2e5
 
-    is_extinct = False
-    for step in range(num_steps):
-        time = h * step
-        harvest_factor  = 0.0
-        if time > ramp_end:
-            harvest_factor = 1.0
-        elif time > ramp_start:
-            harvest_factor = (time - ramp_start) / (ramp_end - ramp_start)
-        harvest_rate = harvest_factor * maximum_harvest_rate
+    results = []
+    
+    for ramp_start in numpy.arange(0, end_time + 0.1, 0.1):
+        for ramp_end in numpy.arange(ramp_start,end_time + 0.1, 0.1):
+            total_harvest = 0
+            is_extinct = False
 
-        if is_extinct:
-            fish_next_step = 0.
-        else:
-            fish_next_step = fish[step] + h * (maximum_growth_rate * (1. - fish[step] / carrying_capacity) * fish[step] - harvest_rate)
-            if fish_next_step <= 0.:
-                is_extinct = True
-                fish_next_step = 0.
-        fish[step + 1] = fish_next_step
+            for step in range(num_steps):
+                time = h * step
+                harvest_factor  = 0.0
+                if time > ramp_end:
+                    harvest_factor = 1.0
+                elif time > ramp_start:
+                    harvest_factor = (time - ramp_start) / (ramp_end - ramp_start)
+                harvest_rate = harvest_factor * maximum_harvest_rate
 
-    return fish
+                if is_extinct:
+                    current_harvest = 0
+                    fish_next_step = 0.
+                else:
+                    current_harvest = h * harvest_rate
+                    fish_next_step = fish[step] + h * (maximum_growth_rate * (1. - fish[step] / carrying_capacity) * fish[step] - harvest_rate)
+                    if fish_next_step <= 0.:
+                        is_extinct = True
+                        current_harvest = fish[step]
+                        fish_next_step = 0.
+                fish[step + 1] = fish_next_step
+                total_harvest += current_harvest
+            results.append([ramp_start,ramp_end,total_harvest])
 
-fish = harvest()
+    return results
 
+harvest = total_harvest()
 
-plt.plot(times, fish)
+x = [item[0] for item in harvest]
+y = [item[1] for item in harvest]
+size = [item[2] for item in harvest]
+
+size = [((item - min(size))/ (max(size) - min(size)))*25 for item in size] 
+
+plt.scatter(x,y,c=size, s = size)
 plt.show()
 
 
--- random generator.py
+++ random generator.py
@@ -8,14 +8,20 @@ def monte_carlo_pi():
     x = RNG(seed = 841)
     y = RNG(seed = 204)
 
+    # plt.scatter(x,y,s=0.1,c = (x**2 + y**2)<1)
+    # plt.show()
+
     pi_list = [(x_curr, y_curr) for x_curr, y_curr in zip(x, y)]
+
+    
     pi_list = [1 if (x**2 + y**2) < 1 else 0 for (x,y) in pi_list]
         
+    
     return (sum(pi_list)/ len(pi_list)) * 4
 
 
 
-def RNG(a = 421, c = 291, m = 100000, seed = 715, num_steps = 1000000):
+def RNG(a = 421, c = 291, m = 100000, seed = 715, num_steps = 100000):
     RNG_list = np.zeros(num_steps+1) # time
     RNG_list[0] = seed
     if (m > 0) and (a < m) and (c < m) and (seed < m):
@@ -32,7 +38,7 @@ def plot():
     return_lst = RNG()
     print(sum(return_lst)/ len(return_lst))
     return_lst.sort()
-    plt.hist(return_lst, bins=1000)
+    plt.hist(return_lst, bins=100000)
     plt.show()
 
 
@@ -40,4 +46,4 @@ pi = monte_carlo_pi()
 print("Difference is: " , abs(math.pi - pi))
 print("Calculated pi:", pi)
 
-plot()
+# plot()
